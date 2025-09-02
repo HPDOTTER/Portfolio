20c7ec5eadbd81f2ee0f0a29ab1dc1022178c38f
HPDOTTER <167873974+HPDOTTER@users.noreply.github.com>
2025-09-02 12:05:51 +0200

    First Commit
    Adding Portfolio Project with the basic components, header styled without funtionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    AboveTheFoldComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    PortfolioComponent,
+    ContactFormComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio';
